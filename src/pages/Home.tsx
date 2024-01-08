6f1008126e9ccfda4b04a584e43cbafedc2ab827
RehanAhmed-dotcom <rehanahmed0406@gmail.com>
2024-01-08 23:51:29 +0500

    portfolio setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../main.css";
+const Home = () => {
+  return (
+    <div className='Home'>
+       <div className='Container'>
+        <h1>
+            I am App Developer
+        </h1>
+        </div> 
+        </div>
+  )
+}
+
+export default Home
